f83ea0e07b8b17d70fce1730997add64d2f5c5ed
Tonniky <224966919@qq.com>
2020-07-21 18:07:36 +0800

    the task of leetcode, keep updating.

---
+++ src/Leetcode/jump45.java
@@ -0,0 +1,38 @@
+package Leetcode;
+
+public class jump45 {
+
+
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+
+
+    public int jump1(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+
+}
