17344539982ef9b59acd993ea952359ae56a5ad4
Tonniky <224966919@qq.com>
2020-07-21 12:46:06 +0800

    the task of leetcode, keep updating.

---
+++ src/Leetcode/mergeTwoLists21.java
@@ -0,0 +1,33 @@
+package Leetcode;
+
+public class mergeTwoLists21 {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode (int data) {
+            this.val = data;
+        }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode d = new ListNode(0);
+        ListNode cur = d;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                cur.next=l1;
+                cur =cur.next;
+                l1= l1.next;
+            }else{
+                cur.next =l2;
+                cur =cur.next;
+                l2 =l2.next;
+            }
+        }
+
+        if(l1==null){
+            cur.next=l2;
+        }else{
+            cur.next=l1;
+        }
+        return d.next;
+    }
+}
