c8c71c3408cefb98757a5e71421a6b9feaf054c9
Tonniky <224966919@qq.com>
2020-12-18 21:42:02 +0800

    the task of leetcode, keep updating.

---
+++ src/group_interview/baidu/findCiclePosition.java
@@ -0,0 +1,41 @@
+package group_interview.baidu;
+
+public class findCiclePosition {
+
+    public ListNode {
+        public int val;
+
+        public ListNode (int data) {
+            this.val = data;
+        }
+
+        public ListNode next;
+    }
+
+
+    public ListNode findCirclePosition(ListNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        ListNode n1 = root;
+        ListNode n2 = root;
+
+        while (n2.next != null && n2.next.next != null) {
+            n1 = root.next;
+            n2 = root.next.next;
+            if (n1 == n2) {
+                break;
+            }
+        }
+        if (n2.next == null || n2.next.next == null) {
+            return null;
+        }
+        n2 = root;
+        while (n1 != n2) {
+            n2 = n2.next;
+            n1 = n1.next;
+        }
+        return n2;
+    }
+}
