5183c81121de310c7983687e93f105d4f5f8321f
Tonniky <224966919@qq.com>
2020-12-19 12:09:34 +0800

    the task of leetcode, keep updating.

---
+++ src/ZCYCodingGuide/listNode/removeLastKthNode.java
@@ -0,0 +1,35 @@
+package ZCYCodingGuide.listNode;
+
+public class removeLastKthNode {
+    public class ListNode {
+        public int val;
+        public ListNode (int data) {
+            this.val = data;
+        }
+    }
+
+    public ListNode removeLastKthNode(ListNode node, int k) {
+        if (node == null || k < 1) {
+            return null;
+        }
+        ListNode cur = node;
+        while (cur1 != null) {
+            k--;
+            cur1 = cur1.next;
+        }
+        if (k == 0) {
+            cur = node.next;
+            return cur;
+        }
+        if (k < 0) {
+            cur = node;
+            while (k != 0) {
+                cur = cur.next;
+                k++;
+            }
+            cur.next = cur.next.next;
+            return cur;
+        }
+        return null;
+    }
+}
---
+++ src/ZCYCodingGuide/printCommonPart.java
@@ -0,0 +1,30 @@
+package ZCYCodingGuide;
+
+import java.util.ArrayList;
+
+public class printCommonPart {
+
+
+    public class ListNode {
+        public int val;
+        public ListNode (int data) {
+            this.val = data;
+        }
+    }
+
+    public List<Integer> printCommonsList(ListNode head1, ListNode head2) {
+        List<Integer> res = new ArrayList<>();
+        while (head1 != null && head2 != null) {
+            if (head1 < head2) {
+                head1 = head1.next;
+            } else if (head1 > head2) {
+                head2 = head2.next;
+            } else {
+                head1 = head1.next;
+                head2 = head2.next;
+                res.add(head.val);
+            }
+        }
+        return res;
+    }
+}
