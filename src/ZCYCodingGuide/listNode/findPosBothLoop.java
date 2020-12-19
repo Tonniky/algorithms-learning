4f9c1d24720758b61f6ff9762f5d06f4ec1160b1
Tonniky <224966919@qq.com>
2020-12-19 11:31:23 +0800

    the task of leetcode, keep updating.

--- src/ZCYCodingGuide/listNode/findPosBothLoop.java
+++ src/ZCYCodingGuide/listNode/findPosBothLoop.java
@@ -10,8 +10,8 @@ public class findPosBothLoop {
     }
 
     public ListNode findPosBothLoop(ListNode head1, ListNode loop1, ListNode head2, ListNode loop2) {
-        Node cur1 = head1;
-        Node cur2 = head2;
+        ListNode cur1 = head1;
+        ListNode cur2 = head2;
         if (loop1 == head2) {
             int len1 = 0, len2 = 0;
             while (cur1 != loop1) {
@@ -40,8 +40,9 @@ public class findPosBothLoop {
                 if (loop1 == loop2) {
                     return loop1;
                 }
-
+                cur1 = cur1.next;
             }
+            return null;
         }
     }
 }
